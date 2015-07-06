dc6190593b0b8ae76c5c68aa6825e741b57ec8fd
LunarDevelopment <development.lunar@gmail.com>
2015-07-06 16:21:19 +0100

    integrated iso plugin

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 #Larvae Knight
+
+## Resources
+- https://github.com/lewster32/phaser-plugin-isometric.git
--- gulpfile.js
+++ gulpfile.js
@@ -15,7 +15,8 @@ paths = {
   assets: 'src/assets/**/*',
   css:    'src/css/*.css',
   libs:   [
-    'src/bower_components/phaser-official/build/phaser.min.js'
+    'src/bower_components/phaser-official/build/phaser.min.js',
+    'src/bower_components/phaser-plugin-isometric/dist/phaser-plugin-isometric.min.js'
   ],
   js:     ['src/js/**/*.js'],
   dist:   './dist/'
