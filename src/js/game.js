9f41f077ddfbf75282fc2b58c4082dc4b46b02a8
LunarDevelopment <development.lunar@gmail.com>
2015-07-06 16:44:15 +0100

    grid based movement code in game.js

--- src/js/game.js
+++ src/js/game.js
@@ -9,7 +9,18 @@
     },
 
     update: function () {
-
+      /**
+       * Grid Based Movement taken from http://stackoverflow.com/questions/27785355/moving-a-sprite-along-a-pre-defined-path-in-phaser-io
+       * @param {Function} cursors.left.isDown move along single axis
+       */
+      if (cursors.left.isDown) {
+        game.physics.arcade.moveToXY(
+          sprite, 
+          sprite.body.x - 70, // target x position
+          Phaser.Math.snapTo(sprite.body.y, 70), // keep y position the same as we are moving along x axis
+          250 // velocity to move at
+        );
+      }
     },
 
     onInputDown: function () {
